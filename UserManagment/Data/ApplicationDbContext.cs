1276428e47e1d0230678d5e19f3c19b1ffb3ddf3
SperZ <ZachSperka@gmail.com>
2021-04-21 10:17:58 -0400

    Add project files.

---
+++ UserManagment/Data/ApplicationDbContext.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagment.Models;
+
+namespace UserManagment.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+       
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.HasDefaultSchema("Identity");
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.ToTable(name: "User");
+            });
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Role");
+            });
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable(name: "UserRoles");
+            });
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable(name: "UserClaims");
+            });
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable(name: "UserLogins");
+            });
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+           {
+               entity.ToTable(name: "RoleClaims");
+           });
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable(name: "UserTokens");
+            });
+        }
+
+    }
+}
